08690ce0852bb1a9f905ecf2642dc46cba23c4fe
Zach <zach.adolphe@gmail.com>
2022-09-14 18:22:48 -0600

    added bootstrap layout

--- app/btre/settings.py
+++ app/btre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'pages'
 ]
 
 MIDDLEWARE = [
